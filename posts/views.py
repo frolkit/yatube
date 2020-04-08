0fd0a9f22d08002449cde76befc7801c1c4b67a5
Nikita <frolkit@gmail.com>
2020-04-09 02:21:23 +0300

    создание вьюх Follow и добавление кнопки подписки

--- posts/views.py
+++ posts/views.py
@@ -2,12 +2,12 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.cache import cache_page
 
 from .forms import PostForm, CommentForm
-from .models import Group, Post, Comment
+from .models import Group, Post, Comment, Follow
 
 User = get_user_model()
-
 
 def index(request):
     post_list = Post.objects.order_by("-pub_date").all()
@@ -47,9 +47,13 @@ def profile(request, username):
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
+    following = None
+    if request.user.is_authenticated:
+        following = Follow.objects.filter(user=request.user, author=author)
     return render(request, "profile.html", {"author": author,
                                             "page": page,
-                                            'paginator': paginator})
+                                            'paginator': paginator,
+                                            'following': following})
 
 
 def post_view(request, username, post_id):
@@ -57,16 +61,19 @@ def post_view(request, username, post_id):
     post = get_object_or_404(Post, id=post_id)
     comments = Comment.objects.filter(post_id=post_id).order_by("-created").all()
     number_posts = Post.objects.filter(author=author).all().count()
+    following = None
+    if request.user.is_authenticated:
+        following = Follow.objects.filter(user=request.user, author=author)
     form = CommentForm()
-    #вывод всех комментариев
-    #Создание формы для отправки комментариев
     return render(request, "post.html", {"author": author,
                                          "post": post,
                                          "number_posts": number_posts,
                                          "form": form,
-                                         "comments": comments})
+                                         "comments": comments,
+                                         "following": following})
 
 
+@login_required
 def add_comment(request, username, post_id):
     form = CommentForm(request.POST)
     if form.is_valid():
@@ -91,9 +98,41 @@ def post_edit(request, username, post_id):
     return render(request, "new_post.html", {'form': form, 'post': post})
 
 
+@login_required
+def follow_index(request):
+    follows = Follow.objects.filter(user=request.user)
+    autors = [follow.author for follow in follows]
+    post_list = Post.objects.filter(author__in=autors).order_by("-pub_date").all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, "follow.html", {'page': page,
+                                           'paginator': paginator})
+
+
+@login_required
+def profile_follow(request, username):
+    user = request.user
+    author = get_object_or_404(User, username=username)
+    follow = Follow.objects.filter(user=request.user, author=author).first()
+    if user != author and follow == None:
+        Follow.objects.create(user=user, author=author)
+    return redirect("profile", username=username)
+
+@login_required
+def profile_unfollow(request, username):
+    user = request.user
+    author = get_object_or_404(User, username=username)
+    follow = Follow.objects.filter(user=user, author=author).first()
+    if follow != None:
+        follow.delete()
+    return redirect("profile", username=username)
+
+
 def page_not_found(request, exception):
         return render(request, "misc/404.html", {"path": request.path}, status=404)
 
 
 def server_error(request):
         return render(request, "misc/500.html", status=500)
+
