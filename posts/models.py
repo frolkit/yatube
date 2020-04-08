c1f387dcd3a82f585bbe62cb355495cf417c2ed9
Nikita <frolkit@gmail.com>
2020-04-09 02:18:16 +0300

    создание модели Follow и добавление related_name для модели Comment

--- posts/models.py
+++ posts/models.py
@@ -21,7 +21,11 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     text = models.TextField()
     created = models.DateTimeField("date published", auto_now_add=True)    
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
