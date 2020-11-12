c5f65361b950fd9be1cb793c9eb5bb2873e76331
frolkit <frolkit@gmail.com>
2020-11-12 16:27:12 +0300

    correct models

--- posts/models.py
+++ posts/models.py
@@ -8,25 +8,35 @@ class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
-    def __str__(self): return self.title
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.ForeignKey(Group, on_delete=models.SET_NULL, blank=True, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts")
+    group = models.ForeignKey(
+        Group, on_delete=models.SET_NULL, blank=True, null=True)
     image = models.ImageField(upload_to='posts/', blank=True, null=True)
-    def __str__(self): return self.text
+
+    def __str__(self):
+        return self.text
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments")
     text = models.TextField()
     created = models.DateTimeField("date published", auto_now_add=True)
 
 
 class Follow(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following")
